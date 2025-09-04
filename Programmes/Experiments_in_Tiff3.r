df1a4228741e85173db0af3b28c15816029524b8
James Hogan - FAME <jamesh@spc.int>
2025-09-04 14:29:25 +1200

    Update tiff3 for deep learning

--- Programmes/Experiments_in_Tiff3.r
+++ Programmes/Experiments_in_Tiff3.r
@@ -27,6 +27,7 @@
 ##                Sentinel-2 sensors.
 ##
 ##                https://registry.opendata.aws/deafrica-geomad/b
+##                https://keras.io/
 ##
 ##    Author:     James Hogan, Senior Marine Resource Economist, 1 September 2025
 ##
@@ -83,16 +84,24 @@
       ##
       ##    Create the layers
       ##
-         network <- keras_model_sequential() %>% 
-                    layer_dense(units = 512, activation = "relu", input_shape = c(28 * 28)) %>%
-                    layer_dense(units = 10, activation = "softmax")
+#         network <- keras_model_sequential() %>% 
+#                    layer_dense(units = 512, activation = "relu", input_shape = c(28 * 28)) %>%
+#                    layer_dense(units = 10, activation = "softmax")
+                    
+         network <- keras_model_sequential()
+         layer_dense(network, units = 512, activation = "relu", input_shape = c(28 * 28))
+         layer_dense(network, units =  10, activation = "softmax")                    
 
       ##
       ##    Define the loss function and the optimiser and the metrics to monitor during training (accuracy)
       ##
-         network %>% compile(optimizer = "rmsprop",
-                             loss = "categorical_crossentropy",
-                             metrics = c("accuracy"))
+#         network %>% compile(optimizer = "rmsprop",
+#                             loss = "categorical_crossentropy",
+#                             metrics = c("accuracy"))
+         compile(network,
+                 optimizer = "rmsprop",
+                 loss = "categorical_crossentropy",
+                 metrics = c("accuracy"))
 
       ##
       ##    Reshape the images so rather than a 28 x 28 array, it becomes a 1 X 784 vector
@@ -117,14 +126,196 @@
       ##
       ## Train the model
       ##
-         network %>% fit(train_images, train_labels, epochs = 5, batch_size = 128)
+#         network %>% fit(train_images, train_labels, epochs = 5, batch_size = 128)
+        fit(network, train_images, train_labels, epochs = 5, batch_size = 128)
          
       ##
       ## Test the model
       ##
-         metrics <- network %>% evaluate(test_images, test_labels)
+#         metrics <- network %>% evaluate(test_images, test_labels)
+         metrics <- evaluate(network, test_images, test_labels)
 
       ##
       ## Predict the model
       ##
-         network %>% predict_on_batch(test_images[1:10,])
+        predict_on_batch(network, test_images[1:10,])
+
+
+##
+##    IMDB Examples - Sentiment classification of text into positive or negative reviewas
+##
+   imdb <- dataset_imdb(num_words = 10000)
+   
+   train_data <- imdb$train$x
+   train_labels <- imdb$train$y
+   test_data <- imdb$test$x
+   test_labels <- imdb$test$y
+
+   vectorize_sequences <- function(sequences, dimension = 10000) 
+                           {
+                             results <- matrix(0, nrow = length(sequences), ncol = dimension)      
+                             for (i in 1:length(sequences))
+                               results[i, sequences[[i]]] <- 1                                     
+                             results
+                           }
+
+   x_train <- vectorize_sequences(train_data)
+   x_test  <- vectorize_sequences(test_data)
+
+   y_train <- as.numeric(train_labels)
+   y_test  <- as.numeric(test_labels)
+
+   ##
+   ##    Overfitting example - the training loss descreases and accuracy increase with each epoch, 
+   ##      but the validation accuracy and loss increase rather than decrease showing the "more accurate" model is 
+   ##      overfitting the data rather than capturing relevant detail.
+   ##
+
+   model <- keras_model_sequential() %>%
+            layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
+            layer_dense(units = 16, activation = "relu") %>%
+            layer_dense(units = 1, activation = "sigmoid")
+ 
+   model %>% compile(optimizer = "rmsprop",
+                     loss = "binary_crossentropy",
+                     metrics = c("accuracy"))
+                     
+   val_indices       <- 1:10000
+   
+   x_val             <- x_train[val_indices,]
+   partial_x_train   <- x_train[-val_indices,]
+   y_val             <- y_train[val_indices]
+   partial_y_train   <- y_train[-val_indices]
+
+   history <- model %>% fit(partial_x_train,
+                            partial_y_train,
+                            epochs = 20,
+                            batch_size = 512,
+                            validation_data = list(x_val, y_val))
+   results <- model %>% evaluate(x_test, y_test)
+   results
+   plot(history)
+   
+   ##
+   ##    Retraining the model with fewer epochs to reduce overfitting. Number of epochs based on above plot
+   ##
+   
+   model <- keras_model_sequential() %>%
+            layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
+            layer_dense(units = 16, activation = "relu") %>%
+            layer_dense(units = 1, activation = "sigmoid")
+
+   model %>% compile(optimizer = "rmsprop",
+                    loss = "binary_crossentropy",
+                    metrics = c("accuracy"))
+
+   history <- model %>% fit(x_train, y_train, epochs = 5, batch_size = 512)
+   results <- model %>% evaluate(x_test, y_test)
+   results
+   plot(history)
+
+##
+##    Reuters Examples - Classifying text into 46 mutually exclusive topics
+##
+   reuters <- dataset_reuters(num_words = 10000)
+   c(c(train_data, train_labels), c(test_data, test_labels)) %<-% reuters
+   length(train_data)
+   length(test_data)
+   
+   ##
+   ##    decoding back to words
+   ##
+      word_index <- dataset_reuters_word_index()
+      reverse_word_index <- names(word_index)
+      names(reverse_word_index) <- word_index
+      
+      decoded_newswire <- sapply(train_data[[1]], function(index) {
+        word <- if (index >= 3) reverse_word_index[[as.character(index - 3)]]   
+        if (!is.null(word)) word else "?"
+      })
+   
+   ##
+   ##    Recode the data into tensors
+   ##
+      vectorize_sequences <- function(sequences, dimension = 10000) {
+        results <- matrix(0, nrow = length(sequences), ncol = dimension)
+        for (i in 1:length(sequences))
+          results[i, sequences[[i]]] <- 1
+        results
+      }
+
+      x_train <- vectorize_sequences(train_data)         
+      x_test <- vectorize_sequences(test_data)
+      
+   ##
+   ##    Recode the data into tensors
+   ##
+      one_hot_train_labels <- to_categorical(train_labels)
+      one_hot_test_labels <- to_categorical(test_labels)
+
+   ##
+   ##   the softmax activation outputs a probability distribution for the 46 label catagories
+   ##
+
+      model <- keras_model_sequential() %>%
+               layer_dense(units = 64, activation = "relu", input_shape = c(10000)) %>%
+               layer_dense(units = 64, activation = "relu") %>%
+               layer_dense(units = 46, activation = "softmax")
+
+      model %>% compile(optimizer = "rmsprop",
+                        loss = "categorical_crossentropy",
+                        metrics = c("accuracy"))
+      val_indices <- 1:1000
+
+      x_val <- x_train[val_indices,]
+      partial_x_train <- x_train[-val_indices,]
+
+      y_val <- one_hot_train_labels[val_indices,]
+      partial_y_train = one_hot_train_labels[-val_indices,]
+      
+      history <- model %>% fit(partial_x_train,
+                               partial_y_train,
+                               epochs = 20,
+                               batch_size = 512,
+                               validation_data = list(x_val, y_val))
+      results <- model %>% evaluate(x_test, one_hot_test_labels)
+      results
+   plot(history)
+   ##
+   ##   Overfitting after 10 - cut down epochs
+   ##
+
+      model <- keras_model_sequential() %>%
+               layer_dense(units = 64, activation = "relu", input_shape = c(10000)) %>%
+               layer_dense(units = 64, activation = "relu") %>%
+               layer_dense(units = 46, activation = "softmax")
+
+      model %>% compile(optimizer = "rmsprop",
+                        loss = "categorical_crossentropy",
+                        metrics = c("accuracy"))
+      val_indices <- 1:1000
+
+      x_val <- x_train[val_indices,]
+      partial_x_train <- x_train[-val_indices,]
+
+      y_val <- one_hot_train_labels[val_indices,]
+      partial_y_train = one_hot_train_labels[-val_indices,]
+      
+      history <- model %>% fit(partial_x_train,
+                               partial_y_train,
+                               epochs = 9,
+                               batch_size = 512,
+                               validation_data = list(x_val, y_val))
+      results <- model %>% evaluate(x_test, one_hot_test_labels)
+      results
+   plot(history)
+   
+   ##
+   ##   Estimate the proportion of something..?
+   ##
+      test_labels_copy <- test_labels
+      test_labels_copy <- sample(test_labels_copy)
+      length(which(test_labels == test_labels_copy)) / length(test_labels)
+      
+   predictions <- model %>% predict(x_test)
+
